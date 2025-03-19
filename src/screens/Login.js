81923ff6388fd431164fea689d45b428fb38d888
guinegrijo <guilhermenegrijo@gmail.com>
2025-03-18 23:37:24 -0300

    tela de login funcional

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,13 +1,58 @@
+import React, { useState } from "react";
 import { View, Text, TextInput, TouchableOpacity, Alert, StyleSheet, Button, Image } from "react-native";
-
+import api from "../axios/axios"
 
 export default function Login({ navigation }) {
+
+    const [user, setUser] = useState({
+        email: '',
+        password: ''
+    })
+
+    async function handleLogin() {
+        await api.postLogin(user)
+            .then(
+                (response) => {
+                    console.log(response.data.message)
+                    // navigation.navigate("Home")
+                }, (error) => {
+                    console.log(error)
+                    Alert.alert(error.response.data.error)
+                }
+            )
+    }
+
+
     return (
         <View>
             <Image source={require("../../assets/logo_senai.png")} style={styles.logo} />
+
+            <Text>Seja bem-vindo(a). Faça o login para acessar a agenda senai ou cadastre-se como novo usuário.</Text>
+
+            <View>
+                <TextInput
+                    placeholder="E-mail"
+                    value={user.email}
+                    onChangeText={(value) => { setUser({ ...user, email: value }) }}
+                    style={styles.input}
+                />
+                <TextInput
+                    placeholder="Senha"
+                    value={user.password}
+                    onChangeText={(value) => { setUser({ ...user, password: value }) }}
+                    style={styles.input}
+                />
+            </View>
+
+            <View>
+                <TouchableOpacity onPress={handleLogin}>
+                    <Text>Entrar</Text>
+                </TouchableOpacity>
+            </View>
+
             <View style={styles.viewNavigate}>
                 <Text >Não possui conta?</Text>
-                <TouchableOpacity onPress={()=> navigation.navigate("Cadastro")}>
+                <TouchableOpacity onPress={() => navigation.navigate("Cadastro")}>
                     <Text style={styles.TextNavigate} >Clique aqui</Text>
                 </TouchableOpacity>
             </View>
@@ -19,22 +64,21 @@ export default function Login({ navigation }) {
 
 
 const styles = StyleSheet.create({
-viewNavigate: {
-    display: "flex",
-    flexDirection: "row",
-    alignSelf: "center",
-},
-TextNavigate: {
-    color: "#215299",
-    marginLeft: '10'
-},
-logo: {
-    width: 300,
-    height: 300,
-    alignSelf: "center",
-    resizeMode: "contain",// Mantém a proporção e evita cortes
-    marginBottom: '-30',
-    marginTop: '-100'
-}
-    
+    logo: {
+        width: 300,
+        height: 300,
+        alignSelf: "center",
+        resizeMode: "contain",// Mantém a proporção e evita cortes
+        marginBottom: '-30',
+        marginTop: '-100'
+    },
+    viewNavigate: {
+        display: "flex",
+        flexDirection: "row",
+        alignSelf: "center",
+    },
+    TextNavigate: {
+        color: "#215299",
+        marginLeft: '10'
+    },
 })
